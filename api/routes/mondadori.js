a7c077528a1db3e9ce6ff3fdca5cf3a0efd1dc69
Adrià Martí Blasco <adria.marti.blasco@gmail.com>
2018-06-19 23:59:09 +0200

    Handle errors

--- api/routes/mondadori.js
+++ api/routes/mondadori.js
@@ -2,6 +2,7 @@ const express = require('express');
 const request = require('request');
 const cheerio = require ('cheerio');
 const router = express.Router();
+const errors = require('../../errors/errors')
 
 router.get('/:isbn', (req, res, next) => {
   const baseUrl = `https://www.mondadoristore.it/search/?g=`
@@ -11,20 +12,22 @@ router.get('/:isbn', (req, res, next) => {
     if (!error && response && response.statusCode == 200) {
       const $ = cheerio.load(body);
       const $wrapper = $('.product-data');
-      const price = $wrapper.find('.new-price strong').html().split(`<`)[0];
-      const priceDecimals = $wrapper.find('.new-price strong .decimals').html().split(`<`)[0];
+      const price = $wrapper.find('.new-price strong').html();
+      const priceDecimals = $wrapper.find('.new-price strong .decimals').html();
       const availability = $wrapper.find('.lightGreen strong').html() ? true : false;
       const link = response.request.uri.href;
 
-      const bookData = {
-        price: `${price}${priceDecimals}`,
-        availability: availability,
-        link: link
+      if (price !== null && priceDecimals !== null) {
+        res.status(200).json({
+          price: `${price.split(`<`)[0]}${priceDecimals.split(`<`)[0]}`,
+          availability: availability,
+          link: endpointUrl
+        });
+      } else {
+        res.json(errors.productNotAvailable());
       }
-
-      res.status(200).json(bookData);
     } else {
-      res.status(200).json({});
+      res.json(errors.productNotAvailable());
     }
   })
 });
