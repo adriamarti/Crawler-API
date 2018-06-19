adb580893f3686bce173d91fb956a55ee68ca790
Adrià Martí Blasco <adria.marti.blasco@gmail.com>
2018-06-19 21:36:44 +0200

    Add handling error logic on non available endpoints

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const libraccioRoutes = require('./api/routes/libraccio');
-const mondadoriRoutes = require('./api/routes/mondadori')
+const mondadoriRoutes = require('./api/routes/mondadori');
 
+// Logs requests
+app.use(morgan('dev'));
+
+// Routes that handle requests
 app.use('/libraccio', libraccioRoutes);
 app.use('/mondadori', mondadoriRoutes);
+
+// Handle errors
+app.use((req, res, next) => {
+  const error = new Error(`Not Found`);
+  error.status = 404;
+  next(error)
+})
 
 module.exports = app;
